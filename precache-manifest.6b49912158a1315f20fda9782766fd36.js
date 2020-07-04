04baf5aaacab91a9f91419ee16726ef1626eec9f
ValkoBob <valko_bob@ukr.net>
2020-07-04 20:06:36 +0300

    Updates

---
+++ precache-manifest.6b49912158a1315f20fda9782766fd36.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c88221e7cfc786bfb2c4126e2f7b8574",
+    "url": "/todo-list/index.html"
+  },
+  {
+    "revision": "7cadbcc6864819ff0b35",
+    "url": "/todo-list/static/css/2.764ccc25.chunk.css"
+  },
+  {
+    "revision": "4977fe4f692cecd3d8f5",
+    "url": "/todo-list/static/css/main.c5491d2e.chunk.css"
+  },
+  {
+    "revision": "7cadbcc6864819ff0b35",
+    "url": "/todo-list/static/js/2.8456c1fe.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/todo-list/static/js/2.8456c1fe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4977fe4f692cecd3d8f5",
+    "url": "/todo-list/static/js/main.0dade240.chunk.js"
+  },
+  {
+    "revision": "4c207b602d40809001a5",
+    "url": "/todo-list/static/js/runtime-main.2929926a.js"
+  }
+]);
